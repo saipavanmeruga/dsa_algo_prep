01d66ef19bddcb62f89fcfc6ea642fe9768afb71
saipavanmeruga <saipavan.meruga@gmail.com>
2024-01-30 23:40:05 -0500

    chore:math concepts

--- maths/maths.cpp
+++ maths/maths.cpp
@@ -3,14 +3,14 @@
 int numberOfDigits(int n)
 {
     int num1 = (int)(log10(n) + 1);
-    // int count = 0;          
-    // while(n > 0)
-    // {
-    //     num1 = n % 10;
-    //     std::cout<<num1<<std::endl;
-    //     count += 1;
-    //     n = n/10;
-    // }
+    int count = 0;          
+    while(n > 0)
+    {
+        num1 = n % 10;
+        std::cout<<num1<<std::endl;
+        count += 1;
+        n = n/10;
+    }
     return num1;    
 
 }
