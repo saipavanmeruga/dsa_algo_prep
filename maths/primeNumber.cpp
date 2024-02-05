888a8e94d797f9dbd31c98f905e748f6154e41e9
saipavanmeruga <saipavan.meruga@gmail.com>
2024-02-04 22:03:23 -0500

    chore:prime number logic

---
+++ maths/primeNumber.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+int main()
+{
+    int num;
+    std::cin>>num;
+    
+    int count = 0;
+
+    //brute force approach
+    for(int i = 1; i <= num; i++)
+    {
+        if(num % i == 0)
+        {
+            count += 1;
+        }
+    }
+    if(count == 2) std::cout<<"prime number"<<std::endl;
+    else std::cout<<"not a prime number"<<std::endl;
+
+    // int count = 0;
+    // // sqrt method
+    // for(int i = 1; i*i <=num; i++)
+    // {
+    //     if(num % i == 0)
+    //     {
+    //         count ++;
+    //         if((num/i) != i)
+    //         {
+    //             count ++;
+    //         }
+    //     }
+    // }
+    // if(count == 2) std::cout<<"prime number"<<std::endl;
+    // else std::cout<<"not a prime number"<<std::endl;
+
+}
