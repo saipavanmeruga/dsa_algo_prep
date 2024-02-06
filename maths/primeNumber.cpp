ccb764d9064c3bbed99d19575ee2c7cf9d7cec3e
saipavanmeruga <saipavan.meruga@gmail.com>
2024-02-06 18:22:02 -0500

    chore: primenumber square root logic

--- maths/primeNumber.cpp
+++ maths/primeNumber.cpp
@@ -18,20 +18,20 @@ int main()
     if(count == 2) std::cout<<"prime number"<<std::endl;
     else std::cout<<"not a prime number"<<std::endl;
 
-    // int count = 0;
-    // // sqrt method
-    // for(int i = 1; i*i <=num; i++)
-    // {
-    //     if(num % i == 0)
-    //     {
-    //         count ++;
-    //         if((num/i) != i)
-    //         {
-    //             count ++;
-    //         }
-    //     }
-    // }
-    // if(count == 2) std::cout<<"prime number"<<std::endl;
-    // else std::cout<<"not a prime number"<<std::endl;
+    int count = 0;
+    // sqrt method
+    for(int i = 1; i*i <=num; i++)
+    {
+        if(num % i == 0)
+        {
+            count ++;
+            if((num/i) != i)
+            {
+                count ++;
+            }
+        }
+    }
+    if(count == 2) std::cout<<"prime number"<<std::endl;
+    else std::cout<<"not a prime number"<<std::endl;
 
 }
